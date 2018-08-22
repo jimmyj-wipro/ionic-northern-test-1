225cabedcf0c2de03a2df34029a02f2bcaaac360
James McMillan <james.mcmillan@wipro.com>
2018-08-22 16:11:47 +0100

    Maps js api - no tabs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 
+declare var google;
+
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  start = 'london, gb';
+  end = 'london, gb';
+  directionsService = new google.maps.DirectionsService;
+  directionsDisplay = new google.maps.DirectionsRenderer;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad(){
+    this.initMap();
+  }
+
+  initMap() {
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 8,
+      disableDefaultUI: true,
+      center: {lat: 51.5074, lng: 0.1278}
+    });
+
+    this.directionsDisplay.setMap(this.map);
+  }
+
+  calculateAndDisplayRoute() {
+    this.directionsService.route({
+      origin: this.start,
+      destination: this.end,
+      travelMode: 'DRIVING'
+    }, (response, status) => {
+      if (status === 'OK') {
+        this.directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
   }
 
 }
